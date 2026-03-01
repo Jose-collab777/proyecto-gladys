2da9a0016e814409c9f80d370364008fa327860e
jose-collab777 <ja772766@gmail.com>
2026-03-01 14:22:04 -0700

    1

--- proyecto-gladys/src/App.jsx
+++ proyecto-gladys/src/App.jsx
@@ -2,12 +2,5 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./login.jsx";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Ruta inicial: muestra el login al abrir la app */}
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Login />;
 }
